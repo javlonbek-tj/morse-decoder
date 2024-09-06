1196937c7fd4336d2fabda78f7697c6cb33bcb8b
Жавлон Турдиматов <j.turdimatov@dkp.local>
2024-09-06 20:11:05 +0500

    feat: add decode logic

--- src/index.js
+++ src/index.js
@@ -1,46 +1,72 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+  const encodedStrings = [];
+  for (let i = 0; i < expr.length; i += 10) {
+    const encodedString = expr.slice(i, i + 10);
+    encodedStrings.push(encodedString);
+  }
+  return encodedStrings
+    .map((encodedString) => {
+      let result = '';
+      if (encodedString === '**********') {
+        result += ' ';
+        return result;
+      }
+      const parsed = parseInt(encodedString).toString();
+      let morseCode = '';
+      for (let i = 0; i <= parsed.length; i += 2) {
+        const sliced = parsed.slice(i, i + 2);
+        if (sliced === '11') {
+          morseCode += '-';
+        }
+        if (sliced === '10') {
+          morseCode += '.';
+        }
+      }
+      result += MORSE_TABLE[morseCode];
+      return result;
+    })
+    .join('');
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
